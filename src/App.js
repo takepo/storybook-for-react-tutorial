2b394f879075b65eafa17d37e3b12b16bd6d7fa4
YokogoshiNahoko <takeshima.nahoko+1@gmail.com>
2020-01-26 14:27:34 +0900

    refactor: componentのディレクトリ整理

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Provider } from "react-redux";
 import store from "./lib/redux";
 
-import InboxScreen from "./InboxScreen";
+import InboxScreen from "./components/InboxScreen";
 
 class App extends Component {
   render() {
